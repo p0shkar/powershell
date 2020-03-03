7e2796ca8ad7db7bb77874559187cbea45b4de81
Oskar Norén <oskar.noren@edu.gotland.se>
2020-03-03 16:58:33 +0100

    Created script to find Basic Auth logins

---
+++ Office365/Get-BasicAuthLogins.ps1
@@ -0,0 +1,56 @@
+<#
+.NAME
+    Get-BasicAuthLogins
+.SYNOPSIS
+    Get recent logins from the Office 365 Security & Compliance Audit log which authenticates using Basic/Legacy Authentication.
+.DESCRIPTION
+    Get recent logins from the Office 365 Security & Compliance Audit log which authenticates using Basic/Legacy Authentication.
+
+    Please suggest to me if you have a better name than "AuthMethod" and "DeviceOrProtocol" for those properties.
+
+    Disclaimer: In the current version it is not totally clear if the search parameters used in this script returns 100% of
+    basic auth types and if all of the returned auth types are indeed Basic Auth. However, modern auth types seems to reside
+    in the operation "UserLoggedIn" and get workload "AzureActiveDirectory".
+
+    This script uses ExchangeOnlineManagement (Exchange Online PowerShell V2) module to connect to EXO.
+    This is still in Preview, use V1 if you prefer.
+    https://docs.microsoft.com/en-us/powershell/exchange/exchange-online/exchange-online-powershell-v2/exchange-online-powershell-v2
+
+    This script uses the ImportExcel module to export the results, remove this and uncomment the rows above to print to screen or
+    export to CSV instead. https://github.com/dfinke/ImportExcel
+.NOTES
+    Version:        1.0
+    Author:         Oskar Noren
+    Creation Date:  2020-03-03
+#>
+
+#----[ CONNECT TO EXO ]----#
+#require module: ExchangeOnlineManagement
+Connect-ExchangeOnline
+
+#----[ VARIABLES ]----#
+[int]$DaysAgoStart = 1
+[int]$DaysAgoEnd = 0
+[int]$ResultSize = 5000
+$Operations = "MailboxLogin" #, "UserLoggedIn"
+$FileName = Join-Path -Path "C:\Temp\" -ChildPath "$(Get-Date -f "yyMMdd-HHmmss")-AuditLog" #Join-Path -Path $env:Temp -ChildPath "$(Get-Date -f "yyMMdd-HHmmss")-AuditLog"
+$StartDate = ((Get-Date).AddDays(-$DaysAgoStart)).ToUniversalTime()
+$EndDate = ((Get-Date).AddDays(-$DaysAgoEnd)).ToUniversalTime()
+
+#----[ GET RESULT ]----#
+$SearchResult = Search-UnifiedAuditLog -StartDate $StartDate -EndDate $EndDate -Formatted -ResultSize $ResultSize -Operations $Operations
+$FilteredResult = $SearchResult | Select-Object -ExpandProperty AuditData | ConvertFrom-Json |
+    Select CreationTime, Operation, RecordType, ResultStatus, Workload, UserId,
+        @{N="AuthMethod";E={(($_.ClientInfoString -split "=",0)[1] -split ";")[0]}},
+        @{N="DeviceOrProtocol";E={(($_.ClientInfoString -split "=",2)[1] -split ";",2).TrimStart(" ")[1]}}
+
+#----[ REPORT CONSOLE ]----#
+#$FilteredResult | ft -AutoSize -Wrap
+
+#----[ REPORT CSV ]----#
+#$FilteredResult | Export-CSV -Path ($FileName + ".csv" ) -NoTypeInformation -Encoding Unicode
+#& $FilePath
+
+#----[ REPORT XLSX ]----#
+#require module: ImportExcel
+$FilteredResult | Export-Excel -Path ($FileName + ".xlsx") -WorksheetName $FileName -AutoFilter -AutoSize -FreezeTopRow -BoldTopRow -Show
