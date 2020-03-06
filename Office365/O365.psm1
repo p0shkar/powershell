ea3ada9b8adf0d4a1fd6ba51331f7852798b68b8
Oskar Norén <oskar.noren@edu.gotland.se>
2020-03-06 10:11:36 +0100

    Added Test-AADConnection and renamed similar functions for EXO and MSOL to same name format

--- Office365/O365.psm1
+++ Office365/O365.psm1
@@ -1,15 +1,25 @@
-function Get-MSOLSessionState {
+function Test-MSOLConnection {
     Get-MsolDomain -ErrorAction SilentlyContinue | Out-Null
     $Result = $?
     return $Result
 }
 
-function Get-EXOSessionState {
+function Test-EXOConnection {
     if (!(Get-PSSession | ? { $_.ComputerName -eq "outlook.office365.com" -and $_.ConfigurationName -eq "Microsoft.Exchange" -and $_.State -eq "Opened" })) {
         return $false
     }
     else{
         return $true
+    }
+}
+
+function Test-AADConnection {
+    try{
+        $SessionInfo = Get-AzureADCurrentSessionInfo -ErrorAction SilentlyContinue
+        return $true
+    }
+    catch{
+        return $false
     }
 }
 
